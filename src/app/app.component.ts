1c3154e4a8a53df655e762694832873bf1155a7a
oleg.bezkorovaynyi <oleg.bezkororvaynyi@globallogic.com>
2017-05-29 20:38:53 +0300

    creating form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
-  singupForm: FormGroup;
+  singUpForm: FormGroup;
+
+  ngOnInit() {
+    this.singUpForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male')
+    });
+  }
 }
